620e5c43644d0dfe74c26030a8b2cf0c6ff7324a
petyo <petyopetkov@gmail.com>
2023-10-03 00:23:12 +0200

    gradle update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,9 +62,8 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.3")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material3:material3:1.2.0-alpha08")
-
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    implementation("androidx.compose.runtime:runtime-livedata:1.5.2")
+    implementation("androidx.compose.runtime:runtime-livedata:1.6.0-alpha06")
 
     // Gson
     implementation ("com.google.code.gson:gson:2.10.1")
@@ -73,7 +72,7 @@ dependencies {
     implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation ("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation(platform("androidx.compose:compose-bom:2023.09.02"))
 
     // google camera scanner
     implementation ("com.google.android.gms:play-services-code-scanner:16.1.0")
@@ -85,9 +84,9 @@ dependencies {
     implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
 
     //dager hilt
-    implementation ("com.google.dagger:hilt-android:2.47")
+    implementation ("com.google.dagger:hilt-android:2.48")
     implementation("com.google.firebase:firebase-crashlytics-buildtools:2.9.9")
-    kapt ("com.google.dagger:hilt-android-compiler:2.47")
+    kapt ("com.google.dagger:hilt-android-compiler:2.48")
     kapt ("androidx.hilt:hilt-compiler:1.0.0")
     implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
 
@@ -97,15 +96,15 @@ dependencies {
     ksp("androidx.room:room-compiler:2.5.2")
     implementation("androidx.room:room-ktx:2.5.2")
 
-    implementation ("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.5.0")
-    kapt ("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.5.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.7.0")
+    kapt ("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.7.0")
 
     testImplementation("junit:junit:4.13.2")
     testImplementation("androidx.room:room-testing:2.5.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.09.02"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.0-alpha06")
 }
