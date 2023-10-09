d51b6dbd01e1e414cde030233401b00649ec55b9
petyo <petyopetkov@gmail.com>
2023-10-10 00:27:03 +0200

    ui changes

--- app/src/main/java/com/example/lotterybalance/presentation/secondScreen/InfoBalanceCard.kt
+++ app/src/main/java/com/example/lotterybalance/presentation/secondScreen/InfoBalanceCard.kt
@@ -10,8 +10,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Text
