268069584b16fba473c85d24d3ee88c012a84267
petyo <petyopetkov@gmail.com>
2023-10-27 15:10:39 +0200

    ui changes

--- app/src/main/java/com/example/lotterybalance/presentation/firstScreen/LazyFila.kt
+++ app/src/main/java/com/example/lotterybalance/presentation/firstScreen/LazyFila.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.AbsoluteRoundedCornerShape
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
